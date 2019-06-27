ccb93824c107f470e8fd16929a8a12fc72ae7343
Caleb Myers <cmyers5108@gmail.com>
2019-06-26 23:46:40 -0400

    working on adding correct input to a string

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ console.log(wordAnswer);
 var answerBlanks = document.getElementById("answerBlanks");
 
 for (var i = 0; i < wordAnswer.length; i++) {
-    answerBlanks.textContent += "_ ";
+    answerBlanks.textContent += "_"
 }
 
 var letterGuess = document.getElementById("letterGuess");
@@ -15,23 +15,21 @@ var letterGuess = document.getElementById("letterGuess");
 document.onkeypress = function (event) {
     letterGuess.textContent = event.key;
 
-    console.log(event.key);
-
     for (var j = 0; j < wordAnswer.length; j++) {
         if (event.key === wordAnswer[j]) {
             console.log("Match!");
 
-            
+            var toBeReplaced = answerBlanks.textContent[j]
+
+            console.log(event.key)
+            console.log(toBeReplaced)
+
+            answerBlanks.textContent = answerBlanks.textContent.replace(answerBlanks.textContent[j], event.key)
+
+            console.log(answerBlanks.textContent)
+            console.log(answerBlanks.textContent[j])
         }
     }
 }
 
-
-
-// letterGuess = ...
-
-//  for (i = 0; i < wordAnswer.length; i++) {
-//      if (letterGuess === wordAnswer[i]) {
-
-//      }
-//  }
+// special bug on targaryan
