d6b7117b2c00506850542f2dc5fb3f05dd6dc3b4
Caleb Myers <cmyers5108@gmail.com>
2019-06-28 20:30:29 -0400

    updated game functionality, edited index.html layout, added images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,17 @@
-var wordBank = ["stark", "targaryan", "lannister", "baratheon", "greyjoy", "martell", "tyrell", "arryn"];
-var wordAnswer = wordBank[Math.floor(Math.random() * wordBank.length)];
+var wordBank = ["stark", "targaryen", "lannister", "baratheon", "greyjoy", "martell", "tyrell", "arryn", "tully"];
 
-console.log(wordAnswer);
+var wordAnswer = wordBank[Math.floor(Math.random() * wordBank.length)]
+console.log(wordAnswer)
 
-var answerBlanks = [];
+
+if (wordAnswer === "arryn" || wordAnswer === "tully" || wordAnswer === "tyrell") {
+    document.getElementById("imageHolder").src = "assets/images/" + wordAnswer + ".webp"
+}
+else {
+    document.getElementById("imageHolder").src = "assets/images/" + wordAnswer + ".png"
+}
+
+var answerBlanks = []
 var textBlanks = document.getElementById("textBlanks")
 
 for (var i = 0; i < wordAnswer.length; i++) {
@@ -11,55 +19,126 @@ for (var i = 0; i < wordAnswer.length; i++) {
     textBlanks.textContent += "_"
 }
 
-var remainingLetters = wordAnswer.length;
-var guessArray = document.getElementById("guessArray");
+var remainingLetters = wordAnswer.length
+
 var guesses = []
-var lives = 12
+var guessArray = document.getElementById("guessArray")
+
+var lives = 10
 var livesLeft = document.getElementById("livesLeft")
 livesLeft.textContent = lives
+
 var winScreen = document.getElementById("winScreen")
 
+var instructions = document.getElementById("instructions")
+var gameBox = document.getElementById("gameBox")
+var replay = document.getElementById("replay")
+
+var winCount = 0
+var winText = document.getElementById("winText")
+winText.textContent = winCount
+
+
+function checkLetter (event) {
+    var guess = event.key
+    var correct = false
+    
+    for (var j = 0; j < wordAnswer.length; j++) {
+        if (answerBlanks[j] === "_") {
+            if (wordAnswer[j] === guess) {
+                if (j === 0) {
+                    guess = guess.toUpperCase()
+                }
+
+                answerBlanks[j] = guess
+
+                var noCommas = [answerBlanks.join("")];
+                textBlanks.textContent = noCommas
+
+                remainingLetters--
+
+                correct = true
+            }            
+        }
+    }
+
+    if (correct === false) {
+        lives--
+        livesLeft.textContent = lives
+    }
+
+    if (lives === 0) {
+        winScreen.textContent = "You lost!!!"
+        replay.style.display = "block"
+    }
+
+    if (remainingLetters > 0) {
+        guesses.push(guess)
+        guessArray.textContent = guesses
+    }
+
+    if (remainingLetters === 0) {
+        winCount++
+        winText.textContent = winCount
+        winScreen.textContent = "You win!!!"
+        replay.style.display = "block"
+    }
+}
+
+function resetGame (event) {
+    wordAnswer = wordBank[Math.floor(Math.random() * wordBank.length)]
+    console.log(wordAnswer)
+
+    if (wordAnswer === "arryn" || wordAnswer === "tully" || wordAnswer === "tyrell") {
+        document.getElementById("imageHolder").src = "assets/images/" + wordAnswer + ".webp"
+    }
+    else {
+        document.getElementById("imageHolder").src = "assets/images/" + wordAnswer + ".png"
+    }
+
+    answerBlanks = []
+    textBlanks.textContent = ""
+
+    for (var i = 0; i < wordAnswer.length; i++) {
+        answerBlanks[i] = "_"
+        textBlanks.textContent += "_"
+    }
+
+    guesses = []
+    guessArray.textContent = ""
+
+    remainingLetters = wordAnswer.length
+    
+    lives = 10
+    livesLeft.textContent = lives
+
+    winScreen.textContent = ""
+
+    replay.style.display = "none"
+}
+
+function gameState (event) {
+    gameBox.style.display = "block"
+    winCounter.style.display = "block"
+    instructions.style.display = "none"
+    replay.style.display = "none"           
+
+    document.onkeypress = function (event) {
+        if (remainingLetters > 0 && lives > 0) {           
+            checkLetter(event)     
+        }
+        
+        else if (event.key === 'y' || event.key === 'Y') {
+            resetGame(event)
+        }
+    }
+}
 
 document.onkeypress = function (event) {
-    if (remainingLetters > 0 && lives > 0) {
-        var guess = event.key
-        var correct = false
+    var userInput = event.key    
+    userInput = userInput.toUpperCase()    
 
-        for (var j = 0; j < wordAnswer.length; j++) {
-            if (answerBlanks[j] === "_") {
-                if (wordAnswer[j] === guess) {
-                    if (j === 0) {
-                        guess = guess.toUpperCase()
-                    }
-                    
-                    answerBlanks[j] = guess
-                    
-                    var noCommas = [answerBlanks.join("")];
-                    textBlanks.textContent = noCommas
-
-                    remainingLetters--
-
-                    correct = true
-                }      
-            } 
-        }
-
-        if (correct === false) {
-            lives--
-            livesLeft.textContent = lives
-        }
-
-        if (lives === 0) {
-            winScreen.textContent = "You lost!!!"
-        }
-
-        if (remainingLetters > 0) {        
-            guesses.push(guess)
-            guessArray.textContent = guesses
-        }
-
-        if (remainingLetters === 0) {
-            winScreen.textContent = "You win!!!"
-        }
+    if (userInput === 'S') {
+        gameState(event)
     }
 }
